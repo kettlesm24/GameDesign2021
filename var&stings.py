6ec6c6371a205ba95eebf5a6acd5351af52553f4
Max <kettlesm24@greenhill.org>
2021-06-07 23:00:04 -0500

    menu+math commit

---
+++ var&stings.py
@@ -0,0 +1,22 @@
+#Max Kettles 6-7
+#Working with strings & diff var
+#while True:
+phrase="hello world"
+num1=13
+num2=3.5
+num3=344234324204952855646541325641965454561564616846331854655555555555554444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444
+var1="dog "
+var2="cat "
+var3="mouse "
+#print (type (num1)) #identify type of data
+#print (type (num2))
+#print (type (num3))
+
+#print(phrase[6:]*3)
+#print(var1 +var2)
+#print(var2 +var3)
+#print(var1 + var2 + var3)
+
+while "world" in phrase:
+    print ("world" in phrase)
+    phrase="changed" 
